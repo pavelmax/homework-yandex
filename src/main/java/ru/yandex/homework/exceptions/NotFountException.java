dd5a1a7d3124deffecdb45578e6d440ecda15cb2
Pavel M <pavelmaxqwerty@gmail.com>
2019-09-10 16:31:53 +0300

    notfoundexception: deleted msg

--- src/main/java/ru/yandex/homework/exceptions/NotFountException.java
+++ src/main/java/ru/yandex/homework/exceptions/NotFountException.java
@@ -3,11 +3,8 @@ package ru.yandex.homework.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.Date;
-
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class NotFountException extends RuntimeException {
-    private Date createAt = new Date();
 
     public NotFountException() {
         super();
